b6116150eb91a50a9aca743389dfdbf204f14917
Administrator <lukas@stylianou.info>
2016-11-09 11:43:48 +0000

    XRect geht alles, XSqr macht noch problmeme

--- CS5001-p4-vector/src/shapes/XRect.java
+++ CS5001-p4-vector/src/shapes/XRect.java
@@ -9,47 +9,29 @@ import gui.Config;
 
 public class XRect extends XShape implements Serializable {
 
-// int x1 = 0;
-// int x2 = 0;
-// int y1 = 0;
-// int y2 = 0;
-// int w = 0;
-// int h = 0;
-
  public XRect() {
-  //  shape = new Rectangle2D.Double();
   shape = new Path2D.Double();
  }
 
  @Override
  public void dragTo(int x, int y) {
-  //  Rectangle2D.Double rect = ((Rectangle2D.Double) shape);
-  //  rect.setRect(x - rect.getWidth() / 2, y - rect.getHeight() / 2, rect.getWidth(), rect.getHeight());
   draw(x - width / 2, y - height / 2, x + width / 2, y + height / 2);
  }
 
  @Override
  public void resize(int x, int y, int corner) {
 
-  //  Rectangle2D.Double rect = ((Rectangle2D.Double) shape);
-
   if (corner == Config.HIT_BOTTOM_RIGHT) {
-   //   rect.setRect(rect.getX(), rect.getY(), x - rect.getX(), y - rect.getY());
-   //   draw((int) rect.getX(), (int) rect.getY(), x, y);
-      draw(x1, y1, x, y);
-//   draw((int) shape.getBounds().getMinX(), (int) shape.getBounds().getMinY(), x, y);
+   draw(x1, y1, x, y);
   }
   else if (corner == Config.HIT_TOP_LEFT) {
-   //   rect.setRect(x, y, rect.getMaxX() - x, rect.getMaxY() - y);
    draw(x, y, x2, y2);
 
   }
   else if (corner == Config.HIT_TOP_RIGHT) {
-   //   rect.setRect(rect.getX(), y, x - rect.getX(), rect.getMaxY() - y);
    draw(x1, y, x, y2);
   }
   else if (corner == Config.HIT_BOTTOM_LEFT) {
-   //   rect.setRect(x, rect.getY(), rect.getMaxX() - x, y - rect.getY());
    draw(x, y1, x2, y);
   }
 
@@ -75,20 +57,6 @@ public class XRect extends XShape implements Serializable {
   this.height = y2 - y1;
   shape = rect;
 
-  //  Rectangle2D.Double rect = ((Rectangle2D.Double) shape);
-  //
-  //  if (x2 < x1 && y2 < y1) {
-  //   rect.setRect(x2, y2, x1 - x2, y1 - y2);
-  //  }
-  //  else if (x2 < x1 && y2 >= y1) {
-  //   rect.setRect(x2, y1, x1 - x2, y2 - y1);
-  //  }
-  //  else if (x2 >= x1 && y2 < y1) {
-  //   rect.setRect(x1, y2, x2 - x1, y1 - y2);
-  //  }
-  //  else if (x2 >= x1 && y2 >= y1) {
-  //   rect.setRect(x1, y1, x2 - x1, y2 - y1);
-  //  }
  }
 
  @Override
--- CS5001-p4-vector/src/shapes/XSquare.java
+++ CS5001-p4-vector/src/shapes/XSquare.java
@@ -2,6 +2,8 @@ package shapes;
 
 import java.awt.geom.Rectangle2D;
 
+import gui.Config;
+
 public class XSquare extends XRect {
 
  public XSquare() {
@@ -11,23 +13,46 @@ public class XSquare extends XRect {
  @Override
  public void draw(int x1, int y1, int x2, int y2) {
 
-  int s = Math.min(Math.abs(x2 - x1), Math.abs(y2 - y1));
-
+  int s = Math.min(Math.abs(x2-x1), Math.abs(y2-y1));
+//  
+//  if (Math.abs(x2-x1) >= Math.abs(y2-y1)) {
+//   s = y2-y1;
+//  }
+//  else {
+//   s = x2-x1;
+//  }
+  
+//  if ()
+  
+  
   int x = x1 > x2 ? x1 - s : x1;
   int y = y1 > y2 ? y1 - s : y1;
 
-  shape = new Rectangle2D.Double(x, y, s, s);
+//  shape = new Rectangle2D.Double(x, y, s, s);
+  super.draw(x,y, x+s, y+s);
 
  }
  
-// @Override
-// public void dragTo(int x, int y) {
-//  
-// }
+ @Override
+ public void dragTo(int x, int y) {
+  super.dragTo(x, y);
+ }
 
  @Override
  public void resize(int x, int y, int corner) {
+  if (corner == Config.HIT_BOTTOM_RIGHT) {
+   draw(x1, y1, x, y);
+  }
+  else if (corner == Config.HIT_TOP_LEFT) {
+   draw(x, y, x2, y2);
 
+  }
+  else if (corner == Config.HIT_TOP_RIGHT) {
+   draw(x1, y, x, y2);
+  }
+  else if (corner == Config.HIT_BOTTOM_LEFT) {
+   draw(x, y1, x2, y);
+  }
  }
 
 }
