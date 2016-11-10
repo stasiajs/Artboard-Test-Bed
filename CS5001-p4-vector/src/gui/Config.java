87d20d4124eba0fef5b05c94d557cbed7eb55538
Administrator <lukas@stylianou.info>
2016-11-10 15:23:09 +0000

    done.

--- CS5001-p4-vector/src/gui/Config.java
+++ CS5001-p4-vector/src/gui/Config.java
@@ -6,42 +6,42 @@ package gui;
  */
 public class Config {
 
- /** The Constant DRAW_MODE. */
+ /** The Constant DRAW_MODE indicates that the draw mode is enabled. */
  public static final int DRAW_MODE = 1;
 
- /** The Constant SELECT_MODE. */
+ /** The Constant SELECT_MODE indicates that the draw mode is enabled. */
  public static final int SELECT_MODE = 2;
 
- /** The Constant DRAW_LINE. */
+ /** The Constant DRAW_LINE indicates that lines can be currently drawn. */
  public static final int DRAW_LINE = 1;
 
- /** The Constant DRAW_RECT. */
+ /** The Constant DRAW_RECT indicates that rectangles can be currently drawn. */
  public static final int DRAW_RECT = 2;
 
- /** The Constant DRAW_SQUARE. */
+ /** The Constant DRAW_SQUARE indicates that squares can be currently drawn. */
  public static final int DRAW_SQUARE = 3;
 
- /** The Constant DRAW_CIRCLE. */
+ /** The Constant DRAW_CIRCLE indicates that circles can be currently drawn. */
  public static final int DRAW_CIRCLE = 4;
 
- /** The Constant DRAW_ELLIPSE. */
+ /** The Constant DRAW_ELLIPSE indicates that ellipses can be currently drawn. */
  public static final int DRAW_ELLIPSE = 5;
 
- /** The Constant DRAW_HEX. */
+ /** The Constant DRAW_HEX indicates that hexagons can be currently drawn. */
  public static final int DRAW_HEX = 6;
 
- /** The Constant HIT_TOP_LEFT. */
+ /** The Constant HIT_TOP_LEFT is the number of the top left resize box. */
  public static final int HIT_TOP_LEFT = 0;
 
- /** The Constant HIT_TOP_RIGHT. */
+ /** The Constant HIT_TOP_RIGHT is the number of the top right resize box. */
  public static final int HIT_TOP_RIGHT = 1;
 
- /** The Constant HIT_BOTTOM_LEFT. */
+ /** The Constant HIT_BOTTOM_LEFT is the number of the bottom left resize box. */
  public static final int HIT_BOTTOM_LEFT = 2;
 
- /** The Constant HIT_BOTTOM_RIGHT. */
+ /** The Constant HIT_BOTTOM_RIGHT is the number of the bottom right resize box. */
  public static final int HIT_BOTTOM_RIGHT = 3;
 
- /** The Constant NOT_HIT. */
+ /** The Constant NOT_HIT is the number if no resize box was hit. */
  public static final int NOT_HIT = 4;
 }
